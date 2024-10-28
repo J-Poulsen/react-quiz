16f33f7bac14c668d205341b8fa3e4fa1b1b0b89
Jens Poulsen <jpoulsen@iworq.com>
2024-10-28 16:55:05 -0600

    add questions server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import DateCounter from './DateCounter';
+import Header from './Header';
+import Main from './Main';
 
 export default function App() {
   return (
     <div className='App'>
-      <DateCounter />
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
